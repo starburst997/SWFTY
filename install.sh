6716028fee8f1346eb0fc80b2a008e6cd0c68c44
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2018-09-24 20:38:34 -0400

    instal script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd submodules
+haxelib dev format format
+haxelib dev haxe-file-save haxe-file-save
+haxelib dev haxe-zip haxe-zip
+haxelib dev hxp hxp
+haxelib dev lime lime
+haxelib dev openfl openfl
+haxelib dev binpacking Rectangle-Bin-Packing
