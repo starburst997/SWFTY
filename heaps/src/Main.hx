111f407581ea4c9be7506a952efc69796c43a15c
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2018-10-14 18:58:43 -0400

    Setting up heaps

--- heaps/src/Main.hx
+++ heaps/src/Main.hx
@@ -1,6 +1,45 @@
 package;
 
-class Main {
+class Main extends hxd.App {
 
-    
+    // TODO: Hack until hxd.System.Platform works
+    var isMobile = #if mobile true #else false #end;
+
+    override function setup() {
+        #if mobile
+        @:privateAccess stage.window.displayMode = sdl.Window.DisplayMode.Fullscreen;
+        #end
+
+        // Looks terrible, doesn't work on iOS
+        //stage.vsync = false;
+        
+        super.setup();
+    }
+
+    override function init() {
+        super.init();
+
+        trace('Hello!');
+
+        
+    }
+
+    override function update(dt:Float) {
+        super.update(dt);
+    }
+
+    override function render(e:h3d.Engine) {
+        super.render(e);
+    }
+
+    static function main() {
+        #if html5
+        // TODO : Download res instead
+        hxd.Res.initEmbed();
+        #else
+        hxd.Res.initLocal();
+        #end
+
+        new Main();
+    }
 }
